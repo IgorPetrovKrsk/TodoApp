8b706ab16a7168970dca296b1d2c7072f818e4fc
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-09-15 09:47:18 -0600

    Initial commit

---
+++ app/src/main/java/igor/petrov/simplecomposecheckbox/MainActivity.kt
@@ -0,0 +1,47 @@
+package igor.petrov.simplecomposecheckbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import igor.petrov.simplecomposecheckbox.ui.theme.SimpleComposeCheckboxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleComposeCheckboxTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimpleComposeCheckboxTheme {
+        Greeting("Android")
+    }
+}
